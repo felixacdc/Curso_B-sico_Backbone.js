9c498afa9694a2cfa441bedb05f235d827a0a6c9
felixacdc <pfmata01@gmail.com>
2016-07-07 22:57:32 -0600

    [add] Curso Backbone.js: Modelos en Backbone.js

---
+++ public/js/backbone/models/libro.js
@@ -0,0 +1,65 @@
+// Crear un model class
+// initialize: es como un metodo constructor
+/*var Libro = Backbone.Model.extend({
+    initialize: function() {
+        console.log('Se ha creado una nueva instancia');
+
+        // la funcion change se ejecuta cada vez que se cambia el modelo
+        this.on('change', function(){
+            console.log('el modelo ha cambiado');
+        });
+    },
+    // defaults crea valores por defecto al hacer una instancia del objeto
+    defaults: {
+        autor: 'Desconocido',
+    }
+});*/
+
+// el metodo toJSON despliega los atributos y el contenido del modelo
+// existen los metodos set y get se accede a ellos de la siguiente manera
+/*
+    modelo.get('atributo');
+    modelo.set('atributo', 'valor'); o
+    modelo.set({
+        'atributo1': 'valor',
+        'atributo2': 'valor'
+    });
+*/
+
+// sincronizacion de modelos: se sincroniza con los datos del servidor para ello definimos la propiedad urlRoot
+// es recomendable no utilizar esta propiedad a menos qeu el modelo no este dentro de una coleccion
+
+var Libro = Backbone.Model.extend({
+    urlRoot: '/libros',
+});
+
+/*
+Optiene los datos del servidor
+var libroUno = new Libro({id:'1',});
+libroUno.fetch();
+
+almacena datos en el servidor
+var libroDos = new Libro({
+    titulo: 'Demian',
+    autor: 'Herman Hesse',
+    categoria: 'literatura', 
+});
+
+libroDos.save();
+
+Editar datos del servidor
+var libroUno = new Libro({id:'1',});
+libroUno.fetch();
+
+libroUno.set({autor: 'desconocido'});
+libroUno.save();
+
+Validacion de datos
+var Libro = Backbone.Model.extend({
+    validate: function(atributos) {
+        if(!atributos.titulo) {
+            return 'debe tener titulo.';
+        }
+    } 
+});
+*/
