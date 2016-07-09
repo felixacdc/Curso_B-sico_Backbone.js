290bdcce88405ae7ceb08c56ffc66158982acd73
felixacdc <pfmata01@gmail.com>
2016-07-09 15:02:37 -0600

    [add] Curso Backbone.js: Vistas o Views en Backbone.js

---
+++ public/js/backbone/views/libreria.js
@@ -0,0 +1,52 @@
+// Creacion de una vista
+// var Libreria = Backbone.View.extend();
+// crear la propiedad "el" desde 0
+/*var Libreria = Backbone.View.extend({
+    tagName: 'span',
+    className: 'nombreClase',
+    id: 'nombreId'
+});*/
+// crear la propiedad "el" desde un elemento existente
+/*var Libreria = Backbone.View.extend({
+    el: '.vista',
+    initialize: function() {
+        this.render();
+    },
+    render: function() {
+        this.$el.html('<h1>Metodo render en accion.</h1>');
+    }
+});*/
+var Libreria = Backbone.View.extend({
+    el: '.vista',
+    events: {
+        'click .cambiarColor': 'cambiarColor'
+    },
+    cambiarColor: function() {
+        this.$el.css('color', 'red');
+    }
+});
+
+// var vista = new Libreria();
+// el objeto principal de una vista es "el" ya que contiene toda la vista dentro de un elemento DOM
+// "el" tiene tres propiedades tagName, id y className
+/*
+ejemplo
+
+var Libreria = Backbone.View.extend({
+    tagName: 'span'
+});
+var vista = new Libreria();
+// cuando no definimos ninguna de las proviedades el valor por defecto del "el" es un div
+vista.el;
+
+// Otra forma de asignar la propiedad "el" a un elemento ya creado en nuestra pagina
+// si se le agrega un $ antes del "el" ($el) optiene las caracteristicas de jquery
+var vista = new Libreria();
+vista.$el;
+vista.$el.text("hola mundo con Backbone.js");
+vista.$el.css("background", "blue");
+vista.$el.append('<strong>Genial</strong>');
+*/
+// el metodo render es el mas importante ya que ayuda a renderizar la vista de nuestra pagina
+// el metodo initialize se registra cada vez que creamos una vista
+// los eventos se registran dentro de un hash la sintaxis es 'Nombre_de_evento selector': 'funcion_callback'
